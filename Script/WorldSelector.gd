b5245571df95bbebf4e363a1ac4df134baf5c108
Phoenix <68404504+PhoenixStroh@users.noreply.github.com>
2025-01-22 14:05:21 -0500

    added bash version of new levels

--- Script/WorldSelector.gd
+++ Script/WorldSelector.gd
@@ -7,21 +7,21 @@ var _game := preload("res://Scene/Game.tscn") as PackedScene
 
 
 func _enter_world(world: String) -> void:
-	global.load_levels(world)
-	global.level = global.firstLevel
-	get_tree().change_scene_to_packed(_game)
-	global.start_music()
+    global.load_levels(world)
+    global.level = global.firstLevel
+    get_tree().change_scene_to_packed(_game)
+    global.start_music()
 
 
 func _on_main_world_selected() -> void:
-	_enter_world(global.DEFAULT_WORLD)
+    _enter_world(global.DEFAULT_WORLD)
 
 
 func _on_extra_worlds_button_pressed() -> void:
-	MainWorldSelector.visible = false
-	ExtraWorldSelector.visible = true
+    MainWorldSelector.visible = false
+    ExtraWorldSelector.visible = true
 
 
 func _on_back_button_pressed() -> void:
-	ExtraWorldSelector.visible = false
-	MainWorldSelector.visible = true
+    ExtraWorldSelector.visible = false
+    MainWorldSelector.visible = true
